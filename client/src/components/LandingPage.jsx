88edb34868ebf37c15e2ed401b5e167ac8f2876d
Gurjot Singh <gurjotsingh@Gurjots-MBP.home>
2018-07-31 21:24:44 -0400

    worked on landing page

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
-
+import './LandingPage.css';
 
 class LandingPage extends Component {
   render(){
     return(
-    <div>
+    <div className='input-btn'>
+      <h1> SIBERIA HILLS </h1>
     <input
       type="text"
       name="e-mail"
       placeholder="Sample@Website.com"
     />
-    <button>Submit</button>
+    <button className='btn'>Submit</button>
+
+    <h3>BACK IN AUGUST</h3>
+    <h3>SIGN UP FOR FIRST UPDATES </h3>
+    <h3>LOVE YOU ALL </h3>
   </div>
 )
   }
